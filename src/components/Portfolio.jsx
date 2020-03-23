b0469e9df4b63a4eed0725508fd0c98388133fc4
tamaramunoz <tami.skt@gmail.com>
2020-03-22 21:43:00 -0300

    calling all projects from API

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -2,26 +2,22 @@ import React, { Fragment,useState, useEffect } from 'react'
 import { Button } from 'react-bootstrap'
 import MyVerticallyCenteredModal from './MyVerticallyCenteredModal';
 
-
 const Portfolio = () => {
 
-    const [ project, setProject ] = useState(null)
+    const [ project, setProject ] = useState([]);
 
     useEffect(() => {
-        console.log('useEffect')
+        // console.log('useEffect')
         getData()
     }, [])
 
     const getData = async () => {
-        const data = await fetch('')
-        const users = await data.json()
-        console.log(users)
+        const data = await fetch('https://raw.githubusercontent.com/tamaramunoz/Portfolio-react/developer/src/json/Projects.json')
+        const infoProject = await data.json()
+        // console.log(infoProject)
+        setProject(infoProject)
 
     }
-
-
-
-
 
     const [ modalShow, setModalShow ] = useState(false);
 
@@ -29,10 +25,24 @@ const Portfolio = () => {
         <Fragment>
             <div>
                 <h2 className="subtitle">Portafolio</h2>
+
+                <ul>
+                    {
+                        project.map(item => {
+                            return (
+                            <div>
+                                <Button variant="primary" onClick={() => setModalShow(true)} key={item.id}>
+                                    {item.title}
+                                </Button>
+                            </div>
+                            )
+                        })
+                    }
+                </ul>
                 <>
-                    <Button variant="primary" onClick={() => setModalShow(true)}>
+                    {/* <Button variant="primary" onClick={() => setModalShow(true)}>
                         Aquí irán las imagenes de mis Proyectos
-                    </Button>
+                    </Button> */}
 
                     <MyVerticallyCenteredModal
                         show={modalShow}
