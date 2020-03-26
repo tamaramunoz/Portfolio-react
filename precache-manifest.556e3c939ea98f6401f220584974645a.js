636e0b14caba9ad3d4effafea8b2a0857190385a
tamaramunoz <tami.skt@gmail.com>
2020-03-26 08:53:23 -0300

    Updates

---
+++ precache-manifest.556e3c939ea98f6401f220584974645a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44e5bec8788cb375ee5bf1dbc41f355b",
+    "url": "/Portfolio-react/index.html"
+  },
+  {
+    "revision": "782cdb7cb500aac3eabc",
+    "url": "/Portfolio-react/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "583819c2a75f137cd86e",
+    "url": "/Portfolio-react/static/css/main.ec07c2e7.chunk.css"
+  },
+  {
+    "revision": "782cdb7cb500aac3eabc",
+    "url": "/Portfolio-react/static/js/2.be846bc5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Portfolio-react/static/js/2.be846bc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "583819c2a75f137cd86e",
+    "url": "/Portfolio-react/static/js/main.0b8f1e3d.chunk.js"
+  },
+  {
+    "revision": "15cc253bb99342eb54b9",
+    "url": "/Portfolio-react/static/js/runtime-main.e2165b32.js"
+  },
+  {
+    "revision": "5db1317f3115785a90e3b9d28bc83c69",
+    "url": "/Portfolio-react/static/media/avatar.5db1317f.png"
+  },
+  {
+    "revision": "ebffe3d0b50c0c155533154882637ea9",
+    "url": "/Portfolio-react/static/media/contactCard-github.ebffe3d0.png"
+  },
+  {
+    "revision": "6ac1a489522fc27ac25133caf7998eac",
+    "url": "/Portfolio-react/static/media/contactCard-gmail.6ac1a489.png"
+  },
+  {
+    "revision": "d93e883459cc6a3bcecfc6f3215edd76",
+    "url": "/Portfolio-react/static/media/contactCard-linkedin.d93e8834.png"
+  }
+]);
