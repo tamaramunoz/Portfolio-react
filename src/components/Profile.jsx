6022f525eff25d37e0ca8f795175eb5f635853e1
tamaramunoz <tami.skt@gmail.com>
2020-04-05 21:39:06 -0400

    adding images and changing hosting on firebase

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -21,7 +21,9 @@ const Profile = () => {
                         className="avatar"
                     />
                 </div>
-                <p className="profileText">Desarrolladora Front-End, disfruto probar nuevas tecnologías por eso es que estoy siempre preparada para aprender e integrar nuevos conocimientos y habilidades. Me caracterizo por tener capacidad de adaptación, compromiso con las actividades que desarrollo y responsabilidad.</p>
+                <p className="profileText">
+                    Front-End developer, disfruto probar nuevas tecnologías por eso es que estoy siempre preparada para aprender e integrar nuevos conocimientos y habilidades. Me gustan las areas de la programación donde pueda seguir explorando mi creatividad y la experimentación. La idea de asumir retos cada vez más grandes me entusiasma, ya que lo considero una buena oportunidad para poner en practica lo que he aprendido y también para seguir fomentando mi curiosidad.
+                </p>
             </div>
         </Fragment>
     );
