976db111fc4ab2db9445956ed2aa8510c581ccae
tamaramunoz <tami.skt@gmail.com>
2020-05-03 21:16:47 -0400

    card in portfolio now are responsives and have more info

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -16,13 +16,13 @@ const Accordions = (props) => {
                 
                   <Accordion.Toggle as={Card.Header} eventKey="1">
                     <Card.Img variant="top" src={item.img} />
-                    <Card.Title>{item.title}</Card.Title>
-                    <Card.Text> {item.description} </Card.Text>
+                    <Card.Title >{item.title}</Card.Title>
+                    <Card.Text> {item.tools} </Card.Text>
                   </Accordion.Toggle>
 
                   <Accordion.Collapse eventKey="1">
                     <Card.Body  className="projectCards">
-                      <Card.Title className="pro-title"> {item.tools} </Card.Title>
+                        <p> {item.description} </p>
                       <Card.Text>
                         <Button className="btn-deploy"><a href={item.url} target="blank" className="btn-deploy-text">Ver Deploy</a></Button>
                         <Button className="btn-repo"><a href={item.repo} target="blank" className="btn-repo-text">Ver Repositorio</a></Button>
