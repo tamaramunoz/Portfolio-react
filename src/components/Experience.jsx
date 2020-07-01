c69c505ad4631d69c522a6da72ede7367d0e2a1e
tamaramunoz <tami.skt@gmail.com>
2020-07-01 13:36:16 -0400

    redeploy and change description in experience component

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -30,7 +30,7 @@ const Experience = () => {
                 <div className="projectsContainer">
 
                     <p className="profileText">
-                        Puedes conocer más a fondo uno a uno los proyectos de esta sección viendo su deploy o repositorio. Desarrollados con tecnologías como Javascript, HTML5, CSS3, NodeJS, React, Bootstrap, Firebase, y entre otras.
+                        Puedes conocer más a fondo uno a uno los proyectos de esta sección viendo su deploy o repositorio. Desarrollados con tecnologías como Javascript, HTML5, CSS3, NodeJS, React, Bootstrap, Firebase, Git and Github, entre otras.
                     </p>
                     
                     <div>
