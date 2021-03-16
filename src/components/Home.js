c8e950457afd669deb4c42a664562d965da34d65
tamara-munoz <tami.skt@gmail.com>
2021-03-16 15:44:57 -0300

    feat: creating a transition with react spring

---
+++ src/components/Home.js
@@ -0,0 +1,51 @@
+import React, { Fragment, useState } from 'react'
+import Background from '../images/fondo-home.png'
+import CVTamara from '../images/Tamara-2020.pdf'
+
+import { useTransition, animated } from 'react-spring'
+
+let sectionStyle = {
+    width: "100%",
+    height: "700px",
+    backgroundImage: `url(${Background})`
+};
+
+const Home = () => {
+
+    const [isVisible, setIsVisible] = useState(false);
+    const transition = useTransition(isVisible, (p) => p, {
+        from: { x: -100, y: 800, opacity: 0, background: 'lightpink'},
+        enter: { x: 0, y: 0, opacity: 1 },
+        leave: { x: 100, y: 800, opacity: 0 },
+    });
+
+
+    return (
+        <Fragment>
+            <div style={sectionStyle} className="home-container">
+                <h2 className="hi">Hola!</h2>
+                <h1 className="im">soy Tamara Muñoz</h1>
+                <h3 className="front"> Front-end Developer </h3>
+
+                <a href={CVTamara} target="blank">
+                    <button className="btn-cv">Ver Curriculum</button>
+                </a>
+
+                <button onClick={() => setIsVisible(v => !v)} >
+                    Clickeame
+                </button>
+
+                <div>
+                    {
+                        transition.map(({item, props, key}) =>
+                            item ? <animated.div style={props} key={key} >Si estoy visible</animated.div> : null
+                        )
+                    }
+                </div>
+
+            </div>
+        </Fragment>
+    );
+}
+
+export default Home;
